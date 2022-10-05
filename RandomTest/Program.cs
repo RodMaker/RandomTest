023ec8873fb97419504fc6448c5d1dfcad141551
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-10-05 17:42:21 +0100

    initial commit

---
+++ RandomTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new System.Random();
+            int randomnumber = rnd.Next(1, 10);
+            Console.WriteLine("randomnumber = {0}", randomnumber);
+            double randomdouble = rnd.NextDouble();
+            Console.WriteLine("randomdouble = {0}", randomdouble);
+        }
+    }
+}
